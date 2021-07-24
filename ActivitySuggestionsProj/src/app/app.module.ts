d747709d468ad71b9d356f9c6eb847f285cff0fc
guramijibladze <guramijibladze@gmail.com>
2021-07-24 22:02:36 +0400

    add ngx translator

---
+++ ActivitySuggestionsProj/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// import { NameApiService, MOVIE_BASE_URL } from './name-api.service';
+import { environment } from 'src/environments/environment';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+export function TranslateHttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, 'assets/i18n/');
+}
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TranslateModule.forRoot(
+      {
+        loader: {
+            provide: TranslateLoader,
+            useFactory: TranslateHttpLoaderFactory,
+            deps: [HttpClient]
+        },
+        defaultLanguage:'ka'
+    })
+  ],
+  providers: [
+    HttpClient],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
