dd8567ab1b81ad98633ab51d80ec48c1c3a63336
guramijibladze <guramijibladze@gmail.com>
2021-07-26 13:10:07 +0400

    create page not-found

--- ActivitySuggestionsProj/src/app/shell/shell.module.ts
+++ ActivitySuggestionsProj/src/app/shell/shell.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from '../shared/shared/shared.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, NotFoundComponent],
   exports: [HeaderComponent],
   imports: [CommonModule, SharedModule]
 })
