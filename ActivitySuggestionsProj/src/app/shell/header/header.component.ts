87690966e21ad4cd492d5856390e9a812c8a721a
guramijibladze <guramijibladze@gmail.com>
2021-07-26 10:27:33 +0400

    added shared module

--- ActivitySuggestionsProj/src/app/shell/header/header.component.ts
+++ ActivitySuggestionsProj/src/app/shell/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  // get isKa(): boolean {
+  //   return this.isLanguage('ka');
+  // }
+
+  constructor(
+    private translateService: TranslateService,
+  ) { }
 
   ngOnInit(): void {
   }
+
+  ka(){
+    this.translateService.use('ka')
+  }
+
+  en(){
+    this.translateService.use('en')
+  }
+  // private isLanguage(lang: string): boolean {
+  //   const defaultLang = this.translateService.defaultLang;
+  //   const currentLang = this.translateService.currentLang;
+
+  //   return currentLang ? currentLang === lang : defaultLang === lang;
+  // }
 
 }
--- ActivitySuggestionsProj/src/app/shell/shell.module.ts
+++ ActivitySuggestionsProj/src/app/shell/shell.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { SharedModule } from '../shared/shared/shared.module';
+
 
 @NgModule({
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, SharedModule]
 })
 export class ShellModule { }
